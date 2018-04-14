b7cd733fb05a5339199554f80f764f4c2b9f5094
youjie <youjie@hudongpai.com>
2018-04-14 15:34:33 +0800

    debug finish

--- event0.0.1.js
+++ event0.0.1.js
@@ -25,7 +25,7 @@ EventEmeitter.prototype.emit = function(type, ...args) {
 //监听type事件
 EventEmeitter.prototype.on = function(type, fn) {
   // 将type事件和对应的fn放入存储池
-  if (this._events.get(type)) {
+  if (!this._events.get(type)) {
     this._events.set(type, fn);
   }
 };
